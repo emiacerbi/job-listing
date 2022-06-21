0766eb0de3ff0d379d92b4bbe283b7340433bcb4
Emiliano Acerbi <eacerbip@gmail.com>
2022-06-21 16:45:20 -0300

    small fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import heroMobile from '../../public/assets/bg-header-mobile.svg'
-import heroDesktop from '../../public/assets/bg-header-desktop.svg'
+// import heroMobile from '../../public/assets/bg-header-mobile.svg'
+// import heroDesktop from '../../public/assets/bg-header-desktop.svg'
 
 export const Header = () => {
   return (
-    <div className='bg-teal-600' >
-      <picture>
-        <source media='(max-width: 400px)' srcSet={heroMobile} />
-        <source media='(min-width: 401px)' srcSet={heroDesktop} />
-        <img src={heroDesktop} alt='hero' />
+    <div className='bg-teal-600 ' >
+      <picture className='w-full'>
+        <source media='(max-width: 400px)' srcSet='bg-header-mobile.svg' />
+        <source media='(min-width: 401px)' srcSet='bg-header-desktop.svg' />
+        <img src='bg-header-desktop.svg' alt='hero' className='w-full' />
       </picture>
     </div>
   )
--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { JobCard } from './JobCard'
-import cross from '../../public/assets/icon-remove.svg'
 
 export const JobList = () => {
   const [arrayOfFilters, setArrayOfFilters] = useState<string[]>([])
@@ -17,8 +16,17 @@ export const JobList = () => {
             <div className='flex flex-row flex-wrap gap-3'>
               {
                 arrayOfFilters.map(el => (
-                  <p className='flex overflow-hidden gap-2 items-center pt-0.5 pl-3 font-bold text-teal-600 bg-teal-50 rounded-md' key={el}>{el}
-                    <img className='p-2 bg-teal-600 hover:bg-neutral-800 cursor-pointer' src={cross} alt='cross' onClick={() => removeFilter(el)} />
+                  <p
+                    className='flex overflow-hidden gap-2 items-center pt-0.5 pl-3 font-bold text-teal-600 bg-teal-50 rounded-md'
+                    key={el}
+                  >
+                    {el}
+                    <img
+                      className='p-2 bg-teal-600 hover:bg-neutral-800 cursor-pointer'
+                      src={'icon-remove.svg'}
+                      alt='cross'
+                      onClick={() => removeFilter(el)}
+                    />
                   </p>)
                 )
               }
